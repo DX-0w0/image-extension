0123816c8b17922581fddd4d82415bc4bb9b1511
DX-0w0 <dong7777xia@gmail.com>
2025-04-23 17:04:30 -0400

    input

--- downloadImages.mjs
+++ downloadImages.mjs
@@ -3,6 +3,7 @@ import path from 'path'
 import axios from 'axios'
 import { pipeline } from 'stream'
 import { promisify } from 'util'
+import { url, totalImages } from './input.mjs'
 
 const streamPipeline = promisify(pipeline)
 
@@ -20,10 +21,6 @@ if (!fs.existsSync(saveDir)) {
 //   'https://media.istockphoto.com/id/2163630620/photo/abstract-minimalist-product-display-with-pink-smoke-and-wooden-branches.webp?s=2048x2048&w=is&k=20&c=Mk2OWS5Wzx4GeMROu0ZykAluoZeNceeSDKqnbA9Rzy8=',
 //   'https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 // ]
-
-// Change url and totalImages according to the need
-const url = 'https://example.com/xa125/1.jpg'
-const totalImages = 3
 
 async function buildImageUrls(url, totalImages) {
   const newUrl = new URL(url)
---
+++ input.mjs
@@ -0,0 +1,5 @@
+// Change url and totalImages according to the need
+const url = 'https://example.com/xa125/1.jpg'
+const totalImages = 3
+
+export { url, totalImages }
