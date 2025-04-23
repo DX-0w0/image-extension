7747945698c0917945806f005b8c170acd751ce6
DX-0w0 <dong7777xia@gmail.com>
2025-04-23 11:21:18 -0400

    dl images

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Gallery Image download 
 Works only for gallery url images that increment the last path by 1 for the image.
+Example
+```
+https://example.com/xa125/1.jpg 
+https://example.com/xa125/2.jpg
+https://example.com/xa125/3.jpg
+https://example.com/xa125/4.jpg
+```
+
 
 # How To
 - run `npm i`
-- run `node downloadImages.js`# Gallery_Image_DL
+- open any gallery image then right click on image to `Open Image in a new tab` and get the url
+- Replace the `url` and `totalImages`
+- run `node downloadImages.js`
---
+++ downloadImages.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const { pipeline } = require('stream')
+const { promisify } = require('util')
+
+const streamPipeline = promisify(pipeline)
+
+// Directory to save images
+const saveDir = path.join(__dirname, 'images')
+
+// Ensure the folder exists
+if (!fs.existsSync(saveDir)) {
+  fs.mkdirSync(saveDir)
+}
+
+// List of image URLs
+// const imageUrls = [
+//     'https://media.istockphoto.com/id/2163630620/photo/abstract-minimalist-product-display-with-pink-smoke-and-wooden-branches.webp?s=2048x2048&w=is&k=20&c=Mk2OWS5Wzx4GeMROu0ZykAluoZeNceeSDKqnbA9Rzy8=',
+//     'https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+// ]
+
+// Change url and totalImages according to the need
+const url = 'https://example.com/xa125/1.jpg'
+const totalImages = 3
+
+async function buildImageUrls(url, totalImages) {
+  const newUrl = new URL(url)
+
+  const parts = newUrl.pathname.split('/')
+  const fileName = parts.pop()
+  const basePath = parts.join('/')
+
+  const baseUrl = `${newUrl.origin}${basePath}`
+  const extension = path.extname(fileName)
+
+  const imageUrls = new Array(totalImages)
+    .fill(baseUrl)
+    .map((url, i) => `${url}/${i + 1}${extension}`)
+  console.log('imageUrls', imageUrls)
+
+  return imageUrls
+}
+
+async function downloadImage(url, index = 0) {
+  try {
+    const fileName = path.basename(new URL(url).pathname)
+    // const fileName = `${index}.jpg`
+    const filePath = path.join(saveDir, fileName)
+    const response = await axios({
+      method: 'get',
+      url,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      },
+      responseType: 'stream',
+    })
+
+    await streamPipeline(response.data, fs.createWriteStream(filePath))
+    console.log(`✅ Saved: ${fileName}`)
+  } catch (err) {
+    console.error(`❌ Failed to download from ${url}:`, err.message)
+  }
+}
+
+async function downloadAllImages(imageUrls) {
+  console.log('🚀 Downloading images...')
+  await Promise.all(imageUrls.map((url, index) => downloadImage(url, index)))
+  console.log('🎉 All images downloaded.')
+}
+
+async function run() {
+  const imageUrls = await buildImageUrls(url, totalImages)
+  downloadAllImages(imageUrls)
+}
+
+run()
